709b4d36207239e4358569bd19b7a5e529b0fa89
Akshithreddy-5 <dammaakshithreddy5@gmail.com>
2026-03-10 09:41:18 +0530

    UC6: Refactored banner using static helper functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
 /**
  * OOPSBannerApp
- * UC3: Render OOPS Banner using String.join()
+ * UC6: Refactor Banner Logic using Static Functions
  *
  * @author Akshith
  * @version 1.0
@@ -10,14 +10,59 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ", "  *****  ", "  *****  ", " ******  ", "  *****  "));
-        System.out.println(String.join(" ", " *     * ", " *     * ", " *     * ", " *     * "));
-        System.out.println(String.join(" ", "*       *", "*       *", " *     * ", "*       *"));
-        System.out.println(String.join(" ", "*       *", "*       *", " ******  ", "*       *"));
-        System.out.println(String.join(" ", "*       *", "*       *", " *       ", "*       *"));
-        System.out.println(String.join(" ", " *     * ", " *     * ", " *       ", " *     * "));
-        System.out.println(String.join(" ", "  *****  ", "  *****  ", " *       ", "  *****  "));
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ", o[i], o[i], p[i], s[i]);
+        }
+
+        // Print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Helper method for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+            "  *****  ",
+            " *     * ",
+            "*       *",
+            "*       *",
+            "*       *",
+            " *     * ",
+            "  *****  "
+        };
+    }
+
+    // Helper method for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+            " ******  ",
+            " *     * ",
+            " *     * ",
+            " ******  ",
+            " *       ",
+            " *       ",
+            " *       "
+        };
+    }
+
+    // Helper method for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+            "  *****  ",
+            " *     * ",
+            "*       *",
+            "  *****  ",
+            "        *",
+            " *     * ",
+            "  *****  "
+        };
     }
 }
-
 
