836c1d20ad4a991051c390e182d0ffcc40653a21
Akshithreddy-5 <dammaakshithreddy5@gmail.com>
2026-03-10 09:01:10 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to console
+ *
+ * @author Akshith
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+}
+java OOPSBannerApp
+
+
