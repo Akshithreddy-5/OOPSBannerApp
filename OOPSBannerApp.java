f928374dfc5210b7c95f3bb5efaf13c1606ece97
Akshithreddy-5 <dammaakshithreddy5@gmail.com>
2026-03-10 21:12:34 +0530

    UC8: Implement HashMap for character patterns and render banner using function

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * OOPSBannerApp
- * UC3: Render OOPS Banner using String.join()
+ * UC8: Use HashMap for Character Patterns and Render via Function
  *
  * @author Akshith
  * @version 1.0
@@ -8,16 +11,77 @@
 
 public class OOPSBannerApp {
 
+    /**
+     * Creates and returns a HashMap containing banner patterns
+     */
+    public static Map<Character, String[]> buildCharacterPatterns() {
+
+        Map<Character, String[]> patterns = new HashMap<>();
+
+        patterns.put('O', new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        });
+
+        patterns.put('P', new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        });
+
+        patterns.put('S', new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "  *****  ",
+                "        *",
+                " *     * ",
+                "  *****  "
+        });
+
+        return patterns;
+    }
+
+    /**
+     * Renders the banner message
+     */
+    public static void renderBanner(String message, Map<Character, String[]> patterns) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+
+                String[] pattern = patterns.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append(" ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ", "  *****  ", "  *****  ", " ******  ", "  *****  "));
-        System.out.println(String.join(" ", " *     * ", " *     * ", " *     * ", " *     * "));
-        System.out.println(String.join(" ", "*       *", "*       *", " *     * ", "*       *"));
-        System.out.println(String.join(" ", "*       *", "*       *", " ******  ", "*       *"));
-        System.out.println(String.join(" ", "*       *", "*       *", " *       ", "*       *"));
-        System.out.println(String.join(" ", " *     * ", " *     * ", " *       ", " *     * "));
-        System.out.println(String.join(" ", "  *****  ", "  *****  ", " *       ", "  *****  "));
+        Map<Character, String[]> patterns = buildCharacterPatterns();
+
+        String message = "OOPS";
+
+        renderBanner(message, patterns);
     }
 }
+
 
 
