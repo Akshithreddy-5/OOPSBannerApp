24d6860aecaee8aa74aba0adfe8d5c3788b068cf
Akshithreddy-5 <dammaakshithreddy5@gmail.com>
2026-03-10 09:30:35 +0530

    UC4: Refactored banner using String array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
 /**
  * OOPSBannerApp
- * UC3: Render OOPS Banner using String.join()
+ * UC4: Render OOPS Banner using String Array and Loop
  *
  * @author Akshith
  * @version 1.0
@@ -10,14 +10,25 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ", "  *****  ", "  *****  ", " ******  ", "  *****  "));
-        System.out.println(String.join(" ", " *     * ", " *     * ", " *     * ", " *     * "));
-        System.out.println(String.join(" ", "*       *", "*       *", " *     * ", "*       *"));
-        System.out.println(String.join(" ", "*       *", "*       *", " ******  ", "*       *"));
-        System.out.println(String.join(" ", "*       *", "*       *", " *       ", "*       *"));
-        System.out.println(String.join(" ", " *     * ", " *     * ", " *       ", " *     * "));
-        System.out.println(String.join(" ", "  *****  ", "  *****  ", " *       ", "  *****  "));
+        // Create String array to store banner lines
+        String[] banner = new String[7];
+
+        banner[0] = String.join(" ", "  *****  ", "  *****  ", " ******  ", "  *****  ");
+        banner[1] = String.join(" ", " *     * ", " *     * ", " *     * ", " *     * ");
+        banner[2] = String.join(" ", "*       *", "*       *", " *     * ", "*       *");
+        banner[3] = String.join(" ", "*       *", "*       *", " ******  ", "*       *");
+        banner[4] = String.join(" ", "*       *", "*       *", " *       ", "*       *");
+        banner[5] = String.join(" ", " *     * ", " *     * ", " *       ", " *     * ");
+        banner[6] = String.join(" ", "  *****  ", "  *****  ", " *       ", "  *****  ");
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+
     }
 }
+
+
 
 
